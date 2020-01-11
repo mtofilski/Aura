50ee22ae6caf1f51ce5a9bb92ceeb3b1b734d165
Marcin Tofilski <mtofilski@gmail.com>
2020-01-12 00:50:43 +0100

    update init migration

--- migrations/init.js
+++ migrations/init.js
@@ -10,7 +10,7 @@ const up = () => {
         modified datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
         PRIMARY KEY (id),
         UNIQUE KEY id (id)
-      ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;`,
+      ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;`,
   );
   connection.execute(
     `CREATE TABLE reward (
@@ -24,7 +24,15 @@ const up = () => {
       ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;`,
   );
   connection.execute(
-    'CREATE TABLE `giver_view` (`id` serial,`giver` varchar(255) NOT NULL, `amount` int NOT NULL DEFAULT 0, `created` datetime NOT NULL DEFAULT NOW(),`modified` datetime NOT NULL DEFAULT NOW(), PRIMARY KEY (id))',
+    `CREATE TABLE giver_view (
+        id bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+        giver varchar(255) NOT NULL,
+        amount int(11) NOT NULL DEFAULT '0',
+        created datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        modified datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        PRIMARY KEY (id),
+        UNIQUE KEY id (id)
+      ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;`,
   );
 };
 
